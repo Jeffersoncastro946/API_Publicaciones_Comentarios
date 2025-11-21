7aa9cdbec5babe3293bfe187e8f3af33d1532681
Jefferson Castro <Jeffersoncastro947@gmail.com>
2025-11-20 23:54:43 -0600

    Endpoint de get comentarios funcional

--- api/services/comentarios.service.js
+++ api/services/comentarios.service.js
@@ -1,5 +1,5 @@
 // src/services/ComentariosService.js
-import mysqlPool from "../config/db/mysql_db.js";
+import pool from "../config/db/mysql_db.js";
 import { sanitize } from "../utils/sanitize.js";
 import { randomUUID } from "crypto";
 
