8b9d7dd3626331431146f94a47f0e679cf45169d
CAR0011 <cesarantonioar9@gmail.com>
2025-11-23 19:01:06 -0600

    Verificacion del codigo de user.model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,19 +1,25 @@
-import { z } from 'zod'
+// api/models/user.model.js
+import { z } from 'zod';
+
+// Esquema para REGISTER
 export const userRegisterModel = z.object({
   name: z
     .string({
       required_error: 'El nombre es obligatorio'
     })
+    .trim()
     .min(3, 'El nombre debe tener al menos 3 caracteres'),
 
   email: z
     .string({
       required_error: 'El correo es obligatorio'
     })
+    .trim()
     .email('El correo no es válido'),
 
   phone: z
     .string()
+    .trim()
     .min(8, 'El teléfono debe tener al menos 8 caracteres')
     .max(20, 'El teléfono es muy largo')
     .optional()
@@ -24,13 +30,15 @@ export const userRegisterModel = z.object({
       required_error: 'La contraseña es obligatoria'
     })
     .min(8, 'La contraseña debe tener al menos 8 caracteres')
-})
+});
 
+// Esquema para LOGIN
 export const userLoginModel = z.object({
   email: z
     .string({
       required_error: 'El correo es obligatorio'
     })
+    .trim()
     .email('El correo no es válido'),
 
   password: z
@@ -38,4 +46,4 @@ export const userLoginModel = z.object({
       required_error: 'La contraseña es obligatoria'
     })
     .min(8, 'La contraseña debe tener al menos 8 caracteres')
-})
+});
